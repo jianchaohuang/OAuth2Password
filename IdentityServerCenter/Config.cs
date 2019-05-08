6a370c861943866e385bc5c3dc2d253281bc67f3
huangjc <huangjc@HUANGJC>
2019-05-08 21:24:01 +0800

    添加项目文件。

---
+++ IdentityServerCenter/Config.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using IdentityServer4;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+
+namespace IdentityServerCenter
+{
+    public class Config
+    {
+        //所有可以访问的Resource
+        public static IEnumerable<ApiResource> GetResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("api","My Api")
+            };
+        }
+
+        //客户端
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client()
+                {
+                    ClientId="client",
+                    AllowedGrantTypes= GrantTypes.ClientCredentials,//模式：最简单的模式
+                    ClientSecrets={//私钥
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes={//可以访问的Resource
+                        "api"
+                    }
+                },
+                new Client()
+                {
+                    ClientId="pwdClient",
+                    AllowedGrantTypes= GrantTypes.ResourceOwnerPassword,//模式：最简单的模式
+                    ClientSecrets={//私钥
+                        new Secret("secret".Sha256())
+                    },
+                    RequireClientSecret=false,//不需要密码
+                    AllowedScopes={//授权的client可以访问范围（scopes）为api的资源
+                        "api"
+                    }
+                }
+            };
+        }
+
+        //测试用户
+        public static List<TestUser> GetTestUsers()
+        {
+            return new List<TestUser>{
+                new TestUser{
+                    SubjectId="1",
+                    Username="wyt",
+                    Password="123456"
+                }
+            };
+        }
+    }
+}
---
+++ ThirdPartyDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net.Http;
+using IdentityModel.Client;
+
+namespace ThirdPartyDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dico = DiscoveryClient.GetAsync("http://localhost:5000").Result;
+
+            //token
+            var tokenClient = new TokenClient(dico.TokenEndpoint, "client", "secret");
+            var tokenResponse = tokenClient.RequestClientCredentialsAsync("api").Result;
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+            Console.WriteLine("\n\n");
+
+
+            var httpClient = new HttpClient();
+            httpClient.SetBearerToken(tokenResponse.AccessToken);
+
+            var response = httpClient.GetAsync("http://localhost:5001/api/values").Result;
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
